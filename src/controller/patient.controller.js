efdba3975d9a708452a247f9cdc1698bd07454d5
Wayne <zweijie1210@outlook.com>
2023-11-02 14:33:32 -0700

    Querys are ready

---
+++ src/controller/patient.controller.js
@@ -0,0 +1,93 @@
+const database = require('../config/mysql.config')
+const Response = require('../domain/response')
+const logger = require('../log/logger')
+const QUERY = require('../query/patient.query')
+
+const HttpStatus = {
+  OK: { code: 200, status: 'OK' },
+  CREATED: { code: 201, status: 'CREATED' },
+  NO_CONTENT: { code: 204, status: 'NO_CONTENT' },
+  BAD_REQUEST: { code: 400, status: 'BAD_REQUEST' },
+  NOT_FOUND: { code: 404, status: 'NOT_FOUND' },
+  INTERNAL_SERVER_ERROR: { code: 500, status: 'INTERNAL_SERVER_ERROR' },
+}
+
+const getPatients = (req, res) => {
+  logger.info(`${req.method} ${req.originalurl}, fetching patients`)
+  database.query(QUERY.SELECT_PATIENTS, (error, results) => {
+    if (!results) {
+      res.status(HttpStatus.OK.code).send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `No patients found`))
+    }
+    else {
+      res.status(HttpStatus.OK.code).send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `Patients retrieved`, { patients: results }))
+    }
+  })
+}
+
+const getPatient = (req, res) => {
+  logger.info(`${req.method} ${req.originalurl}, fetching patient`)
+  database.query(QUERY.SELECT_PATIENT, [req.params.id], (error, results) => {
+    if (!results[0]) {
+      res.status(HttpStatus.NOT_FOUND.code).send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, `Patient by id:${req.params.id} not found`))
+    }
+    else {
+      res.status(HttpStatus.OK.code).send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `Patient retrieved`, results[0]))
+    }
+  })
+}
+
+const createPatient = (req, res) => {
+  logger.info(`${req.method} ${req.originalurl}, creating patient`)
+  database.query(QUERY.CREATE_PATIENT, Object.values(req.body), (error, results) => {
+    if (!results) {
+      logger.error(error.message)
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR.code).send(new Response(HttpStatus.INTERNAL_SERVER_ERROR.code, HttpStatus.INTERNAL_SERVER_ERROR.status, `Error occured`))
+    }
+    else {
+      const patient = { id: results.insertedId, ...req.body, created_at: new Date() }
+      res.status(HttpStatus.CREATED.code).send(new Response(HttpStatus.CREATED.code, HttpStatus.CREATED.status, `Patient created`, { patient }))
+    }
+  })
+}
+
+const updatePatient = (req, res) => {
+  logger.info(`${req.method} ${req.originalurl}, updating patient`)
+  database.query(QUERY.SELECT_PATIENT, [req.params.id], (error, results) => {
+    if (!results[0]) {
+      res.status(HttpStatus.NOT_FOUND.code).send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, `Patient by id:${req.params.id} not found`))
+    }
+    else {
+      database.query(QUERY.UPDATE_PATIENT, [...Object.values(req.body), req.params.id], (error, results) => {
+        if (!error) {
+          res.status(HttpStatus.OK.code).send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `Patient updated`, { id: req.params.id, ...req.body }))
+        }
+        else {
+          logger.error(error.message)
+          res.status(HttpStatus.INTERNAL_SERVER_ERROR.code).send(new Response(HttpStatus.INTERNAL_SERVER_ERROR.code, HttpStatus.INTERNAL_SERVER_ERROR.status, `Error occured`))
+        }
+      })
+    }
+  })
+}
+
+const deletePatient = (req, res) => {
+  logger.info(`${req.method} ${req.originalurl}, deleting patient`)
+  database.query(QUERY.DELETE_PATIENT, [req.params.id], (error, results) => {
+    if (results.affectedRows > 0) {
+      res.status(HttpStatus.OK.code).send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `Patient deleted`, results[0]))
+    }
+    else {
+      res.status(HttpStatus.NOT_FOUND.code).send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, `Patient by id:${req.params.id} not found`))
+    }
+  })
+}
+
+module.exports = {
+  HttpStatus,
+  createPatient,
+  updatePatient,
+  deletePatient,
+  getPatient,
+  getPatients,
+}
+
--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ const express = require('express')
 const ip = require('ip')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const logger = require('./log/logger')
 const Response = require('./domain/response')
+const { HttpStatus } = require('./controller/patient.controller')
 
 dotenv.config()
 const PORT = process.env.PORT || 3030
 const app = express()
 
 app.listen(PORT, () => {
-  console.log('\n\n-----------------------------------------------------------')
-  console.log(`Server is listening on (IP): ${ip.address()}:${PORT}`)
-  console.log('-----------------------------------------------------------\n\n')
+  logger.info(`Server is listening on (IP): ${ip.address()}:${PORT}`)
 })
 
 app.use(cors({ origin: '*' }))
@@ -19,5 +19,5 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-  res.send(new Response(200, 'OK', 'Patient API, v1.0.0 - All System Go'))
+  res.send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Patient API, v1.0.0 - All System Go'))
 })
